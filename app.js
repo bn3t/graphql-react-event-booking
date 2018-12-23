1b349d3056452130e66e475d6ea3b2f174c9b9ee
Bernard Niset <bernard.niset@gmail.com>
2018-12-23 18:47:44 +0100

    Lesson #5

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const graphqlHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
 
-const events = [];
+const Event = require("./models/event");
 
 const app = express();
 
@@ -40,20 +41,48 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return events;
+        return Event.find()
+          .then(result => {
+            return result.map(event => ({
+              ...event._doc,
+              _id: event.id
+            }));
+          })
+          .catch(err => {
+            throw err;
+          });
       },
-      createEvent: args => {
-        const event = {
-          _id: Math.random().toString(),
-          ...args.eventInput,
-          date: new Date().toISOString(),
-          price: +args.eventInput.price
-        };
-        events.push(event);
-        return event;
+      createEvent: ({ eventInput }) => {
+        const event = new Event({
+          title: eventInput.title,
+          description: eventInput.description,
+          price: eventInput.price,
+          date: new Date(eventInput.date)
+        });
+        return event
+          .save()
+          .then(result => {
+            return { ...result._doc, _id: result._doc._id.toString() };
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
       }
     },
     graphiql: true
   })
 );
-app.listen(3000);
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${
+      process.env.MONGO_PASSWORD
+    }@cluster0-psshp.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
