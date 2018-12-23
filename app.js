334af7ea8a53dce0814be868ea93bfc0fb7d7eb4
Bernard Niset <bernard.niset@gmail.com>
2018-12-23 15:23:03 +0100

    Lesson #4

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
+
+let events = ["Romantic Cooking", "Sailing", "All-Night Coding"];
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
 
-app.get("/", (req, res, next) => {
-  res.send("Hello, world!");
-});
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: () => {
+        return events;
+      },
+      createEvent: args => {
+        const eventName = args.name;
+        events = [...events, eventName];
+        return eventName;
+      }
+    },
+    graphiql: true
+  })
+);
 app.listen(3000);
